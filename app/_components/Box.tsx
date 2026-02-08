fff1b51c17a029ec7e6d307c9e2bec10bda34f69
kss2002 <kss2002@users.noreply.github.com>
2026-02-08 14:35:13 +0900

    chore: 주석

--- app/_components/Box.tsx
+++ app/_components/Box.tsx
@@ -1,3 +1,8 @@
+// app/_components/Box.tsx
+// 밑줄 박으면 라우팅으로 인식하지 않음.
+// app에서는 단순히 페이지를 보여주는 화면만 할당하자.
+// 컴포넌트는 비즈니스 로직인 feature 폴더에 넣자.
+
 export default function Box() {
   return <div className="p-4 border">Box</div>;
 }
