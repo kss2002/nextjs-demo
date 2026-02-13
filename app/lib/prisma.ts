506a799cfb79e0f726d37da6a729358f5eadd510
kss2002 <kss2002@users.noreply.github.com>
2026-02-13 18:30:06 +0900

    chore: import문 경로 수정

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@/app/generated/prisma/client';
+import { PrismaClient } from '@/app/generated/prisma';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
